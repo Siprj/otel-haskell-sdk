e87aeb695c75dce3c2a51fbf3c04602ddd95753e
Jan Sipr <sipr.jan@gmail.com>
2022-10-08 22:32:54 +0200

    chore: Remove the failing test

--- test/Tests.hs
+++ test/Tests.hs
@@ -15,7 +15,4 @@ unitTests =
     "Unit tests"
     [ testCase "List comparison (different length)" $
         [1, 2, 3 :: Int] `compare` [1, 2] @?= GT
-    , -- the following test does not hold
-      testCase "List comparison (same length)" $
-        [1, 2, 3 :: Int] `compare` [1, 2, 2] @?= LT
     ]
