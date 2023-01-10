bbff744d6cf8a90067bcbb86225da5bb583bcfa4
Jan Sipr <sipr.jan@gmail.com>
2023-01-10 18:07:49 +0100

    Testing the client library

--- src/Lib.hs
+++ src/Lib.hs
@@ -5,29 +5,72 @@ module Lib
   )
 where
 
-import Network.HTTP.Client
 import Data.ProtoLens
 import Data.ProtoLens.Labels ()
 import Lens.Micro
 import Data.Text
 import Proto.Opentelemetry.Proto.Common.V1.Common
 import Proto.Opentelemetry.Proto.Logs.V1.Logs
+import Otel.Client
+import Proto.Opentelemetry.Proto.Resource.V1.Resource (Resource)
+import Control.Concurrent.STM
+import Control.Monad
+import Control.Concurrent (threadDelay)
 
 greet :: IO ()
 greet = do
-  manager <- newManager defaultManagerSettings
-  initilaRequest <- parseRequest "http://localhost:4218/v1/logs"
-  print $ showMessage message
-  print $ encodeMessage message
-  let request = initilaRequest { method = "POST", requestHeaders = [("Content-Type", "application/x-protobuf")], requestBody = RequestBodyBS $ encodeMessage message }
-  httpLbs request manager >>= print
-
-  putStrLn "asdfasdf"
+  otelClient <- createOtelClient defautOtelClientParameters
+  forever $ sendData otelClient
   where
-    message :: LogRecord
-    message = defMessage
+    sendData :: OtelClient -> IO ()
+    sendData OtelClient{..} = do
+      threadDelay 1
+      atomically $ writeTBQueue queueLogs otelResourceLogs
+
+    otelResourceLogs :: ResourceLogs
+    otelResourceLogs = defMessage
+      & #scopeLogs .~ otelScopedLogs
+      & #resource .~ otelResource
+
+    otelResource :: Resource
+    otelResource = defMessage
+      & #attributes .~ resourceAttributes
+
+    resourceAttributes :: [KeyValue]
+    resourceAttributes =
+      [ defMessage & #key .~ "machine" & #value .~
+        ( defMessage & #kvlistValue .~
+          (defMessage & #values .~
+            [defMessage & #key .~ "name" & #value .~ (defMessage & #stringValue .~ "machine-name")
+            ]
+          )
+        )
+      , defMessage & #key .~ "machine.id" & #value .~ (defMessage & #maybe'stringValue ?~ "123")
+      , defMessage & #key .~ "container_name" & #value .~ (defMessage & #maybe'stringValue ?~ "blabla container")
+      ]
+
+    otelScopedLogs :: [ScopeLogs]
+    otelScopedLogs =
+      [ defMessage & #scope .~ instrScope & #logRecords .~ [logRecord]
+      , defMessage & #scope .~ instrScope & #logRecords .~ [logRecord]
+      ]
+
+    instrScope :: InstrumentationScope
+    instrScope = defMessage
+      & #name .~ "super nice scope 1"
+      & #version .~ "0.0.1"
+      & #attributes .~ scopeAttributes
+
+    scopeAttributes :: [KeyValue]
+    scopeAttributes =
+      [ defMessage & #key .~ "nice.scope.attibute1" & #value .~ (defMessage & #maybe'stringValue ?~ "attribute1")
+      , defMessage & #key .~ "nice.scope.attibute2" & #value .~ (defMessage & #maybe'stringValue ?~ "attribute2")
+      , defMessage & #key .~ "source" & #value .~ (defMessage & #maybe'stringValue ?~ "super source")
+      ]
+
+    logRecord :: LogRecord
+    logRecord = defMessage
       & #severityText .~ ("INFO" :: Text)
-      & #traceId .~ "112313"
       & #body .~ messageBody
 
     messageBody :: AnyValue
--- src/Otel/Client.hs
+++ src/Otel/Client.hs
@@ -1,16 +1,26 @@
 module Otel.Client
   ( createOtelClient
-  , OtelClientParameters(logEnpoint, traceEndpoint, queueSize, queueTimeout)
+  , OtelClientParameters(..)
+  , OtelClient(..)
   , defautOtelClientParameters
   )
 where
 
 import GHC.Generics
+import Data.Vector (fromList)
 import Numeric.Natural
 import Network.HTTP.Client
 import Control.Concurrent.STM.TBQueue
-import Proto.Opentelemetry.Proto.Collector.Logs.V1.LogsService
+import Lens.Micro
+import Proto.Opentelemetry.Proto.Collector.Logs.V1.LogsService (ExportLogsServiceRequest)
+import Proto.Opentelemetry.Proto.Collector.Logs.V1.LogsService_Fields (vec'resourceLogs)
+import Proto.Opentelemetry.Proto.Logs.V1.Logs
 import Proto.Opentelemetry.Proto.Collector.Trace.V1.TraceService
+import Control.Concurrent
+import Control.Monad (void, forever)
+import Control.Concurrent.STM (atomically)
+import Data.ProtoLens (defMessage, encodeMessage)
+import Data.Foldable (forM_)
 
 data OtelClientParameters = OtelClientParameters
   { logEnpoint :: String
@@ -28,7 +38,7 @@ data OtelClientParameters = OtelClientParameters
   -- they start to be drooped in case the logs/traces/metrics are generated to
   -- fast.
   -- Default value: 100000
-  , queueTimeout :: Natural
+  , queueTimeout :: Int
   -- ^ How long to wait for batch processing. Value is in microseconds.
   -- Default value is: 1000000 us (1s).
   }
@@ -45,10 +55,11 @@ defautOtelClientParameters = OtelClientParameters
 data OtelClient = OtelClient
   { httpManager :: Manager
   -- TODO: I don't think using Request is good idea :).
-  , queueLogs :: TBQueue ExportLogsServiceRequest
+  , queueLogs :: TBQueue ResourceLogs
   , queueTraces :: TBQueue ExportTraceServiceRequest
   , logRequestBase :: Request
   , traceRequestBase :: Request
+  , queueTimeout :: Int
   }
 
 
@@ -70,5 +81,45 @@ createOtelClient OtelClientParameters{..} = do
         }
   queueLogs <- newTBQueueIO queueSize
   queueTraces <- newTBQueueIO queueSize
-  pure $ OtelClient {..}
+  let client = OtelClient {..}
+  clientThreadId <- forkIO $ runClientProcess client
+  -- FIXME: This threadId should be used somewhere to be able to terminate the
+  -- logging safely.
+  void $ pure clientThreadId
+  pure client
 
+runClientProcess :: OtelClient -> IO ()
+runClientProcess OtelClient{..} = forever $ do
+  putStrLn "processing queue"
+
+  logMessages <- atomically (flushTBQueue queueLogs)
+  putStrLn  "*****************************************"
+  putStrLn $ "number of logs: " <> show (length logMessages)
+  putStrLn "*****************************************"
+--  let logData = encodeMessage $ logRequest logMessages
+--  httpLbs logRequestBase{requestBody = RequestBodyBS logData } httpManager >>= print
+  mvars <- batchProcessing [] 5000 logMessages
+  waitForThreads mvars
+  traceMessages <- fmap encodeMessage <$> atomically (flushTBQueue queueTraces)
+  putStrLn $ "number of traces: " <> show (length traceMessages)
+  forM_ traceMessages $ \message -> httpLbs traceRequestBase{requestBody = RequestBodyBS message} httpManager >>= print
+  putStrLn "waiting for the queue"
+  threadDelay queueTimeout
+  where
+    logRequest :: [ResourceLogs] -> ExportLogsServiceRequest
+    logRequest logMessages = defMessage & vec'resourceLogs .~ fromList logMessages
+
+    batchProcessing :: [MVar ()] -> Int -> [ResourceLogs] -> IO [MVar ()]
+    batchProcessing mvars _ [] = pure mvars
+    batchProcessing mvars size logs = do
+      let (xs, xss) = splitAt size logs
+
+      mvar <- newEmptyMVar
+      runInFork mvar $ httpLbs logRequestBase{requestBody = RequestBodyBS . encodeMessage $ logRequest xs } httpManager >>= print
+      batchProcessing (mvar : mvars) size xss
+
+    runInFork :: MVar () -> IO () -> IO ()
+    runInFork mvar io = void $ forkFinally io (\_ -> putMVar mvar ())
+
+    waitForThreads :: [MVar ()] -> IO ()
+    waitForThreads = mapM_ readMVar
