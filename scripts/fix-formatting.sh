889a3de7597727b2837201f837ea8baebc0d4202
Giorgio Marinelli <giorgio@marinel.li>
2023-06-16 17:20:31 +0200

    chore: create new otel-protocol package and regenerate protocol files using proto-lens-protoc

--- scripts/fix-formatting.sh
+++ scripts/fix-formatting.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env sh
 set -e
 # speed-up fourmolu formatting ignoring auto-generated files
-git ls-files '*.hs' ':!:generated/**' | xargs -r fourmolu -i -q
+git ls-files '*.hs' ':!:protocol/**' | xargs -r fourmolu -i -q
 git ls-files '*.cabal' | xargs -r cabal-fmt -i
